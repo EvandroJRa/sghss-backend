2f121290da2012d7ccfb680136a1f110f61b3f64
Evandro <evandrojramos@gmail.com>
2025-08-11 09:36:45 -0300

    Commit inicial: Estrutura do projeto Spring Boot

---
+++ src/test/java/com/sghss/SghssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sghss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SghssBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
